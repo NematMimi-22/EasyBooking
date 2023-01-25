74ec911b609175b9b0a7101b1cdb11246e2346a0
NematMimi-22 <106815902+NematMimi-22@users.noreply.github.com>
2023-01-25 15:26:18 +0200

    split users list to users and owner

--- admin/handlers/get-user.php
+++ admin/handlers/get-user.php
@@ -1,5 +1,5 @@
 <?php
-$sql="SELECT id,name,email,password, type, status, created_at FROM users";
+$sql="SELECT id,name,email,password, type, status, created_at FROM users where type='user'";
 //here query that get data from sql
 //sqlResult that going to database and put data in it
 $sqlResult=mysqli_query($conn,$sql);
