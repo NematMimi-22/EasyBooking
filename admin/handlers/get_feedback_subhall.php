62c4e9a96275f6f69fe858fc37805ee01836e177
hussein-mimi <97499197+hussein-mimi@users.noreply.github.com>
2023-03-21 08:19:31 +0200

    fix

--- admin/handlers/get_feedback_subhall.php
+++ admin/handlers/get_feedback_subhall.php
@@ -2,7 +2,7 @@
 $sql="SELECT f.id, f.feedback, u.name
 FROM feedback_subhall f
 INNER JOIN users u
-ON f.user_id = u.id where f.hall_id=73 and  f.feedback != '' ORDER BY f.id DESC";
+ON f.user_id = u.id where f.hall_id=$shallId and  f.feedback != '' ORDER BY f.id DESC";
 //here query that get data from sql
 //sqlResult that going to database and put data in it
 $sqlResult=mysqli_query($conn,$sql);
